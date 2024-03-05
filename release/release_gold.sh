2cbee2bb78a4497422f74a1dfc70eb156afdd4d6
Szantina Szanto <szantina.szanto@liferay.com>
2024-03-05 16:53:39 +0100

    LRP-4846 Check if tag already exists

--- release/release_gold.sh
+++ release/release_gold.sh
@@ -105,27 +105,37 @@ function tag_release {
 		repository=liferay-portal
 	fi
 
-	if (! curl
-			"https://api.github.com/repos/liferay/${repository}/git/tags" \
-			--data-raw '
-				{
-					"message": "",
-					"object": "$(lc_get_property release-data/release.properties git.hash.liferay-portal-ee)",
-					"tag": "${LIFERAY_RELEASE_VERSION}",
-					"type": "commit"
-				}' \
-			--fail \
-			--header "Accept: application/vnd.github+json" \
-			--header "Authorization: Bearer ${LIFERAY_RELEASE_GITHUB_PAT}" \
-			--header "X-GitHub-Api-Version: 2022-11-28" \
-			--max-time 10 \
-			--request POST \
-			--retry 3 \
-			--silent)
+	if (! curl "https://api.github.com/repos/liferay/${repository}/git/refs/tags/${LIFERAY_RELEASE_VERSION}" \
+		--header "Accept: application/vnd.github+json" \
+		--header "Authorization: Bearer ${LIFERAY_RELEASE_GITHUB_PAT}" \
+		--header "X-GitHub-Api-Version: 2022-11-28" \
+		| grep "${LIFERAY_RELEASE_VERSION}")
 	then
-		lc_log ERROR "Unable to tag release."
+		if (! curl "https://api.github.com/repos/liferay/${repository}/git/tags" \
+				--data-raw '
+					{
+						"message": "",
+						"object": "$(lc_get_property release-data/release.properties git.hash.liferay-portal-ee)",
+						"tag": "${LIFERAY_RELEASE_VERSION}",
+						"type": "commit"
+					}' \
+				--fail \
+				--header "Accept: application/vnd.github+json" \
+				--header "Authorization: Bearer ${LIFERAY_RELEASE_GITHUB_PAT}" \
+				--header "X-GitHub-Api-Version: 2022-11-28" \
+				--max-time 10 \
+				--request POST \
+				--retry 3 \
+				--silent)
+		then
+			lc_log ERROR "Unable to tag release."
 
-		return "${LIFERAY_COMMON_EXIT_CODE_SKIPPED}"
+			return "${LIFERAY_COMMON_EXIT_CODE_SKIPPED}"
+		else
+			lc_log DEBUG "Tag has been added"
+		fi
+	else
+		lc_log ERROR "The tag already exists."
 	fi
 }
 
